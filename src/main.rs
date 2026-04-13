06bd31aff9d5c4df603415f7b62de50982458628
cclvi256 <i@cclvi.cc>
2026-04-13 13:47:25 +0800

    feat: Add web backend hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use apple_bot::routes::app_routes;
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    let app = app_routes();
+    let listener = tokio::net::TcpListener::bind("[::]:8888")
+        .await
+        .expect("Failed to bind TCP listener");
+    info!("Listening on: http://[::]:8888");
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to serve HTTP service");
 }
